2140be398338fff5c5e2ef443cf2ec2c323d1cfd
sadiqsaidu <sadiqsaidu1221@gmail.com>
2022-06-20 16:47:15 +0100

    refactoring

--- face_detect.py
+++ face_detect.py
@@ -1,31 +1,35 @@
 import sys 
 import cv2 as cv
 
-# Get user supplied values
-imagePath = sys.argv[1]
-cascPath = "face.xml"
+def main():
+    imagePath = sys.argv[1]
+    cascPath = "face.xml"
+    detect_face(imagePath, cascPath)
 
-# Create the haar cascade
-faceCascade = cv.CascadeClassifier(cascPath)
+def detect_face(image, file):
+    faceCascade = cv.CascadeClassifier(file)
 
-# Read the image
-image = cv.imread(imagePath)
-gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    # Read the image
+    img = cv.imread(image)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv.cv2.CASCADE_SCALE_IMAGE
-)
+    # Face detection
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv.cv2.CASCADE_SCALE_IMAGE
+    )
 
-print("Found {0} faces!".format(len(faces)))
+    print("Found {0} faces!".format(len(faces)))
 
-# Draws a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # draws a rectangle around the faces detected
+    for (x, y, w, h) in faces:
+        cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-cv.imshow("Faces found", image)
-cv.waitKey(0)
+    cv.imshow("Faces found", img)
+    cv.waitKey(0)
+
+if __name__ == '__main__':
+    main()
